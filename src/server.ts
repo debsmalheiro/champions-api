5d031c910b3c038da924e3a8ffdc6d6860ba24e6
Deborah Malheiro <malheirodev@gmail.com>
2025-04-04 17:14:30 -0300

    feat: create server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
-console.log('oi')
+import createApp from "./app";
+
+const app = createApp();
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log('Server running at port ' + port);
+});
