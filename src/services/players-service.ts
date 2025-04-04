d20e85030ed8d6a2f0f0fb1242b141c1de79ae79
Deborah Malheiro <malheirodev@gmail.com>
2025-04-04 17:46:03 -0300

    refactor: change responsability to service

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,6 +1,17 @@
+import {noContent, ok} from "../utils/http-helper";
+
 export const getPlayerService = async () => {
-    return {
+    const data = {
         id: 10,
         name: "CR7",
     };
+    let response = null;
+
+    if (data) {
+        response = await ok(data);
+    } else {
+        response = await noContent();
+    }
+
+    return response;
 }
