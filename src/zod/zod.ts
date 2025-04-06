ee9b176cb6a8e015dad1071d263eb9e0492e6214
Deborah Malheiro <malheirodev@gmail.com>
2025-04-06 17:04:32 -0300

    feat: type inference zod

--- src/zod/zod.ts
+++ src/zod/zod.ts
@@ -4,19 +4,34 @@ const schema = z.object({
     name: z.string().min(2),
     email: z.string().email(),
     age: z.number().min(18).max(120),
+    status: z.boolean(),
+    skills: z.array(
+        z.object({
+            name: z.string(),
+            value: z.number(),
+        })
+    )
 });
 
-let data = {
+type User = z.infer<typeof schema>;
+
+let data: User = {
     name: "Deborah",
     email: "malheirodev@gmail.com",
-    age: 27
+    age: 27,
+    status: true,
+    skills: [
+        {
+            name: "nodejs",
+            value: 5
+        },
+        {
+            name: "react",
+            value: 8
+        }
+    ]
 };
 
-const result = schema.safeParse(data);
+const result = schema.parse(data);
+console.log(result);
 
-if (result.success) {
-    console.log("Success");
-    console.log(result.data);
-} else {
-    console.log("Error")
-}
