d7d58c6b980de0e37a4709638d982deafcbfe228
Deborah Malheiro <malheirodev@gmail.com>
2025-04-05 22:00:14 -0300

    feat: implements cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors";
 
 function createApp() {
     const app = express();
 
     app.use(express.json());
     app.use("/api", router);
+
+    // const corsOptions = {
+    //     origin: ["http://localhost:3333", "http://localhost:8080"],
+    //     method: ["GET", "POST"],
+    // }
+
+    // app.use(cors(corsOptions));
+    app.use(cors());
 
     return app;
 }
