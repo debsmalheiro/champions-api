04a7ac3c26e5a812c76513541667a765cd6f760c
Deborah Malheiro <malheirodev@gmail.com>
2025-04-06 14:19:01 -0300

    feat: transform array in list

--- src/fs-module/file.ts
+++ src/fs-module/file.ts
@@ -2,7 +2,7 @@ import { writeFile } from "fs/promises";
 
 const exec = async () => {
     const dogs = ["Jolie", "Moana", "Maui"];
-    const dogsTxt = dogs.join(", ");
+    const dogsTxt = dogs.join("\n");
 
     await writeFile("./src/fs-module/teste.txt", dogsTxt);
 }
